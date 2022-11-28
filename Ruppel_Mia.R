c079d830a8b5319c6c273ab2a48080eaae11c365
miaruppel <106988503+miaruppel@users.noreply.github.com>
2022-11-28 16:01:18 -0600

    Initial files and elementary code

---
+++ Ruppel_Mia.R
@@ -0,0 +1,24 @@
+# STAT 7110 Big Data Analysis 
+# 11/28/2022
+# Mia Ruppel
+
+rm(list=ls())
+setwd('~/Documents/RScripts')
+
+# load in data 
+bigdata <- read.table('fat.dat.txt')
+
+# convert case numbers to row names
+rownames(bigdata) = bigdata[ ,1]
+bigdata <- bigdata[ ,-1] 
+
+# rename columns 
+col_name <- c('%BF_Brozek', '%BF_Siri', 'Density', 'Age', 'Weight', 'Height', 
+              'Adip_Index', 'Fat_Free_Weight','Neck_Circum', 'Chest_Circum', 'Abd_Circum', 'Hip_Circum', 
+              'Thigh_Circum', 'Knee_Circum', 'Ankle_Circum', 'Ext_Bicep_Circum', 
+              'Forearm_Circum', 'Wrist_Circum')
+colnames(bigdata) <- col_name
+
+
+
+
